8fb2ac49a5ef782324fa476aab4ee44febd02148
Cabané <jorgecabane93@gmail.com>
2016-07-11 15:01:23 -0400

    upload attendance initial commit

--- locallib.php
+++ locallib.php
@@ -8,20 +8,15 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
-
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 /**
- *
-*
-* @package    local
-* @subpackage paperattendance
-* @copyright  2016 Jorge Cabané (jcabane@alumnos.uai.cl) 					
-* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-*/
-//Pertenece al plugin PaperAttendance
+ * @package    local
+ * @subpackage paperattendance
+ * @copyright  2016 Hans Jeria (hansjeria@gmail.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
