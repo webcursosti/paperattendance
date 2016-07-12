c438deedf119338f9b813d03b522cefb5911f2ad
Cabané <jorgecabane93@gmail.com>
2016-07-12 17:01:49 -0400

    upload langs

---
+++ lang/en/local_paperattendance.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Strings for component 'paperattendance', language 'en'
+ *
+ * @package   paperattendance
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$string['notallowedupload']="Not allowed to upload attendances";
+$string['uploadtitle']="PaperAttendance";
+$string['uploadsuccessful']="Attendance correctly uploaded";
+$string['header']="Upload form";
+$string['uploadteacher']="Upload requested by";
+$string['uploadrule']="Required field, must be .pdf";
+$string['uploadplease']="Please select a pdf on the filepicker";
+$string['uploadfilepicker']="Attendance Pdf";
+$string['selectteacher']="Select teacher";
+
--- upload.php
+++ upload.php
@@ -24,56 +24,72 @@
 * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 //Pertenece al plugin PaperAttendance
-require_once(dirname(dirname(dirname(dirname(__FILE__)))) . '/config.php');
+require_once(dirname(dirname(dirname(__FILE__))) . '/config.php');
 require_once($CFG->dirroot . '/local/paperattendance/forms/upload_form.php');
 require_once($CFG->dirroot . '/local/paperattendance/locallib.php');
+require_once ($CFG->dirroot . "/repository/lib.php");
 global $DB, $OUTPUT,$COURSE;
 
 // User must be logged in.
 require_login();
 if (isguestuser()) {
-    die();
+    //die();
 }
-$courseid = required_param('category', PARAM_INT);
+$courseid = required_param('courseid', PARAM_INT);
 // We are in the course context.
-$coursecontext = context_course::instance($courseid);
+$context = context_system::instance();
 // And have viewcostreport capability.
-if (! has_capability('local/paperattendance:uploadattendance', $coursecontext)) {
+if (! has_capability('local/paperattendance:upload', $context)) {
     // TODO: Log invalid access to upload attendance.
     print_error(get_string('notallowedupload', 'local_paperattendance'));
-    die();
+   //	 die();
 }
 // This page url.
 $url = new moodle_url('/local/paperattendance/upload.php', array(
-    'course' => $courseid));
+    'courseid' => $courseid));
 
 $pagetitle = get_string('uploadtitle', 'local_paperattendance');
-$PAGE->set_context($coursecontext);
+$course = $DB ->get_record("course", array("id" =>$courseid));
+
+$PAGE->set_context($context);
 $PAGE->set_url($url);
-$PAGE->set_pagelayout('incourse');
+$PAGE->set_pagelayout('standard');
 $PAGE->set_heading(get_site()->fullname);
-$PAGE->set_title($pagetitle));
+$PAGE->set_title($pagetitle);
+
 // Add the upload form for the course.
-$addform = new upload_form ();
+$addform = new upload_form (null, array("courseid" => $courseid));
 // If the form is cancelled, refresh the instante.
 if ($addform->is_cancelled()) {
     redirect($url);
     die();
 } 
-else if ($data = $addform->get_data()) {
-// If not cancelled
-$content = $data->get_file_content('file');
-$name = $data->get_new_filename('file');
-$file = $data->save_stored_file('file', $coursecontext->id, 'paperattendance', 'tmpupload', $courseid, '/', $name));
-// Validate that file was correctly uploaded.
-if (!$file) {
-	print_error('Could not upload file');
-}
-
+if ($addform->get_data()) {
+	require_capability('local/paperattendance:upload', $context);
+	
+	$data = $addform -> get_data();
+	$teacherid = $data -> teacher;
+	var_dump($teacherid);
+	
+	$path = $CFG -> dataroot. "temp/local/paperattendance/unread";
+	if(!file_exists($path)){
+		mkdir($path, 0777, true);
+	}
+	// Save file
+	$file = $addform->save_file('file', $path."/".$addform->get_new_filename('file'),false);
+	// Validate that file was correctly uploaded.
+	if (!$file) {
+		print_error('Could not upload file');
+	}
+	else{
+	// Display confirmation page before moving out.
+	redirect($url, get_string('uploadsuccessful', 'local_paperattendance'), 3);
+	die();
+	}
 }
 // If there is no data or is it not cancelled show the header, the tabs and the form.
 echo $OUTPUT->header();
-echo $OUTPUT->heading($pagetitle . ' ' . $category->name);
+echo $OUTPUT->heading($pagetitle. " " . $course->shortname . " " . $course->fullname);
 // Display the form.
 $addform->display();
 echo $OUTPUT->footer();
