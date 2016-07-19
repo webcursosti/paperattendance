f8914e1e5e322bfdb6d2a4a0b5e287d8860ce9b6
Cabané <jorgecabane93@gmail.com>
2016-07-19 12:45:19 -0400

    testqr

---
+++ testqr.php
@@ -0,0 +1,56 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+*
+* @package    local
+* @subpackage paperattendance
+* @copyright  2016 Hans Jeria (hansjeria@gmail.com)
+* @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+require_once (dirname(dirname(dirname(__FILE__))) . "/config.php");
+global $DB, $PAGE, $OUTPUT, $USER;
+
+$context = context_system::instance();
+
+
+$urlprint = new moodle_url("/local/paperattendance/testpdfs.php");
+// Page navigation and URL settings.
+$pagetitle = "TEST imagick";
+$PAGE->set_context($context);
+$PAGE->set_url($urlprint);
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title($pagetitle);
+
+echo $OUTPUT->header();
+
+
+$imagick = new Imagick();
+$imagick->setResolution(100,100);
+$imagick->readImage('b1.pdf[0]');
+//$imagick->writeImages('attendance.jpg', false);
+$imagick->setImageFormat('png');
+$imagick->writeImage('b1.png');
+
+$imagick->readImage('b2.pdf[0]');
+//$imagick->writeImages('attendance.jpg', false);
+$imagick->setImageFormat('png');
+$imagick->writeImage('b2.png');
+
+
+echo $OUTPUT->footer();
