528b6c8d56a3e1199bd832f0760854d15916641f
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2016-10-25 17:05:53 -0300

    add capability print to editingteacehr

--- classes/task/processpdf.php
+++ classes/task/processpdf.php
@@ -20,6 +20,7 @@
 *
 * @package    local
 * @subpackage paperattendance
+* @copyright 2016 Hans Jeria <hansjeria@gmail.com>
 * @copyright  2016 Jorge Cabané (jcabane@alumnos.uai.cl) 					
 * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
@@ -36,29 +37,30 @@ class processpdf extends \core\task\scheduled_task {
 	public function execute() {
 		
 		require_once(dirname(dirname(dirname(dirname(dirname(__FILE__))))).'/config.php');
-		require_once($CFG->dirroot . '/local/paperattendance/locallib.php');
-		
+		require_once($CFG->dirroot . '/local/paperattendance/locallib.php');		
 		global $DB;
 		
-			// Sql that brings the unread pdfs names
-			$sqlunreadpdfs = "SELECT  id, pdf AS name, courseid
-			FROM {paperattendance_session}
-			WHERE status = ?
-			ORDER BY lastmodified ASC";
-		
-			// Parameters for the previous query
-			$params = array(PAPERATTENDANCE_STATUS_UNREAD);
-		
-			// Read the pdfs if there is any unread, with readpdf function
-			if($resources = $DB->get_records_sql($sqlunreadpdfs, $params)){
-				$path = $CFG -> dataroot. "/temp/local/paperattendance/unread";
-				foreach($resources as $pdf){
-					$process = paperattendance_readpdf($path, $pdf-> name, $pdf->courseid);
-					if($process){
-						$pdf->status = 1;
-						$DB->update_record("paperattendance_session", $pdf);
-					}
+		// Sql that brings the unread pdfs names
+		$sqlunreadpdfs = "SELECT id, 
+				pdf AS name, 
+				courseid
+				FROM {paperattendance_session}
+				WHERE status = ?
+				ORDER BY lastmodified ASC";
+	
+		// Parameters for the previous query
+		$params = array(PAPERATTENDANCE_STATUS_UNREAD);
+	
+		// Read the pdfs if there is any unread, with readpdf function
+		if($resources = $DB->get_records_sql($sqlunreadpdfs, $params)){
+			$path = $CFG -> dataroot. "/temp/local/paperattendance/unread";
+			foreach($resources as $pdf){
+				$process = paperattendance_readpdf($path, $pdf-> name, $pdf->courseid);
+				if($process){
+					$pdf->status = 1;
+					$DB->update_record("paperattendance_session", $pdf);
 				}
 			}
+		}
 	}
 }
