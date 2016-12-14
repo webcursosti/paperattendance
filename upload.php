101eb3d74219035c0e58fdd02e935fa14b6c0c24
mqueirolo <queiroloco@gmail.com>
2016-12-14 16:28:16 -0300

    Error on upload fixed by Cabane

--- upload.php
+++ upload.php
@@ -86,7 +86,8 @@ if ($addform->get_data()) {
 	$attendancepdffile = $path . "/unread/paperattendance_".$courseid."_".$time.".pdf";
 	
 	//first check if there's a readable QR code 
-	if(paperattendance_get_qr_text($path."/unread/", "paperattendance_".$courseid."_".$time.".pdf") == "error"){
+	//if(paperattendance_get_qr_text($path."/unread/", "paperattendance_".$courseid."_".$time.".pdf") == "error"){
+	if(paperattendance_get_qr_text($path."/unread/", $filename) == "error"){
 		$courseurl = new moodle_url('/course/view.php', array(
 				'id' => $courseid));
 		redirect($courseurl, get_string('couldntreadqrcode', 'local_paperattendance'), 3);
