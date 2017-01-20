fee61b9d087471dda5633c28cdd13874e709abc7
Cabané <jorgecabane93@gmail.com>
2017-01-20 12:36:46 -0300

    capability

--- version.php
+++ version.php
@@ -28,6 +28,6 @@
 defined('MOODLE_INTERNAL') || die();
 $plugin->pluginname = "local_paperattendance";
 $plugin->component = 'local_paperattendance';
-$plugin->version   = '2017011902';
+$plugin->version   = '2017012001';
 $plugin->requires = 2015111600;
 
