f14a29f45197614e24dc6b10468afb1101c4c033
jorgecabane93 <jorgecabane93@gmail.com>
2017-03-13 00:14:51 -0300

    fix

--- printsearch.php
+++ printsearch.php
@@ -102,8 +102,8 @@ $sqlcourses =   "SELECT c.id,
 		INNER JOIN {course_categories} as cat ON (cat.id = c.category)
 		WHERE cat.path like ? AND c.idnumber > 0
 		GROUP BY c.id";
-$ncourses = count($DB->get_records_sql($sqlcourses, array("%/".$path."%")));
-$courses = $DB->get_records_sql($sqlcourses, array("%/".$path,"%"),$page*$perpage, $perpage);
+$ncourses = count_records_sql($sqlcourses, array("%/".$path."%"));
+$courses = $DB->get_records_sql($sqlcourses, array("%/".$path."%"), $page*$perpage, ($page + 1) * $perpage);
 $coursecount = $page*$perpage+1;
 foreach($courses as $course){
 	$printurl = new moodle_url('/local/paperattendance/print.php', array(
