7ff01a8413ab18f40ffb4123394ea3cd3fe1b77a
Jorge Cabané <jorgecabane93@gmail.com>
2017-04-15 21:49:46 -0300

    4

--- quickprint.php
+++ quickprint.php
@@ -90,7 +90,7 @@ if (paperattendance_checktoken($CFG->paperattendance_omegatoken)){
 	
 	$modules = json_encode($result);
 	var_dump($modules);
-	foreach(json_decode($modules) as $module){
+	foreach($modules as $module){
 		echo $module->horaInicio;
 		echo "<br>";
 	}
