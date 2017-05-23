5ddea3081eb87c7ef58e64603509bc3f9036e86a
Jorge Cabané <jorgecabane93@gmail.com>
2017-05-23 14:54:34 -0400

    s

--- locallib.php
+++ locallib.php
@@ -630,7 +630,7 @@ function paperattendance_check_session_modules($arraymodules, $courseid, $time){
 			INNER JOIN {paperattendance_sessmodule} AS sessmodule ON (sessmodule.sessionid = sess.id)
 			WHERE sessmodule.moduleid $sqlin AND sess.courseid = ?  AND sessmodule.date = ? ";
 	
-	$resultado = $DB->get_records_sql ($sessionquery, $parametros );
+	$resultado = $DB->get_record_sql ($sessionquery, $parametros );
 	var_dump($resultado);
 	if(count($resultado) == 0){
 		return "perfect";
