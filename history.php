0ec5becb25baf8b9d6f1145ffce2ac9c3a23cd5d
Jorge Cabané <jorgecabane93@gmail.com>
2017-06-16 16:11:53 -0400

    name

--- history.php
+++ history.php
@@ -352,7 +352,7 @@ if( $isteacher || is_siteadmin($USER) || has_capability('local/paperattendance:p
 
 		$viewerpdf= html_writer::nonempty_tag(
 				"div",
-				$pdf->Output(),
+				$pdf->Output("ession.pdf", "I"),
 				array(
 						"style" => "height:75vh; width:60vw"
 				));
