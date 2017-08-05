584d8d4e5283e4147388bd56d93e654e3ed7ebc1
Matias Queirolo <mqueirolo@alumnos.uai.cl>
2017-08-05 01:23:41 -0400

    fix

--- missingpages.php
+++ missingpages.php
@@ -326,8 +326,8 @@ if ($action == "edit") {
 							.form-control::-ms-input-placeholder { color: lightgrey; }  /* Microsoft Edge *
 							</style>');
 	echo html_writer::div(get_string("missingpageshelp","local_paperattendance"),"alert alert-info", array("role"=>"alert"));
-  	$pdfarea = html_writer::div($viewerpdf,"col-md-9", array( "id"=>"pdfviewer"));
-  	$inputarea = html_writer::div($inputs,"col-md-2 col-md-offset-1 row", array( "id"=>"inputs"));
+  	$pdfarea = html_writer::div($viewerpdf,"col-md-12 row", array( "id"=>"pdfviewer"));
+  	$inputarea = html_writer::div($inputs,"col-md-12 row", array( "id"=>"inputs"));
  	echo html_writer::div($inputarea.$pdfarea, "form-group");
 	
 }
