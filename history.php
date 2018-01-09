af70c440c975d9c7b55cc6690f8a9c4a16b3b976
Matias <queiroloco@gmail.com>
2018-01-09 16:06:30 -0300

    history view order by hour

--- history.php
+++ history.php
@@ -396,7 +396,7 @@ if( $isteacher || is_siteadmin($USER) || has_capability('local/paperattendance:p
 						   INNER JOIN {paperattendance_sessmodule} AS sm ON (s.id = sm.sessionid)
 						   INNER JOIN {paperattendance_module} AS m ON (sm.moduleid = m.id)
 						   WHERE s.courseid = ?
-						   ORDER BY sm.date DESC";
+						   ORDER BY sm.date, hour DESC";
 		
 		$attendances = $DB->get_records_sql($getattendances, array($courseid));
 		
