cf1cf759743ca296199b8ee7ef908fac5e9f1198
Matias <queiroloco@gmail.com>
2018-01-12 11:06:29 -0300

    part 3 fixing discussion

--- discussion.php
+++ discussion.php
@@ -29,6 +29,13 @@ require_once (dirname(dirname(dirname(__FILE__)))."/config.php");
 require_once($CFG->dirroot . '/local/paperattendance/locallib.php');
 require_once ($CFG->dirroot."/local/paperattendance/forms/response_form.php");
 
+//libraries for FPDI
+require_once ($CFG->dirroot . "/repository/lib.php");
+require_once ($CFG->libdir . '/pdflib.php');
+require_once ($CFG->dirroot . '/mod/assign/feedback/editpdf/fpdi/fpdi.php');
+require_once ($CFG->dirroot . "/mod/assign/feedback/editpdf/fpdi/fpdi_bridge.php");
+require_once ($CFG->dirroot . "/mod/assign/feedback/editpdf/fpdi/fpdi.php");
+
 global $DB, $PAGE, $OUTPUT, $USER, $CFG;
 
 $action = optional_param("action", "view", PARAM_TEXT);
