e0230f62d244b84536b7ff1e69480352f6a72fc6
Rommel5055 <javiergonzalez@alumnos.uai.cl>
2018-04-11 20:35:55 -0300

    Debugging error

--- locallib.php
+++ locallib.php
@@ -1244,6 +1244,7 @@ function paperattendance_sendMail($attendanceid, $courseid, $teacherid, $uploade
 			$messagehtml = "<html>";
 			$messagehtml .= "<p>".get_string("dear", "local_paperattendance") ." ". $teacher->firstname . " " . $teacher->lastname . ",</p>";
 			$messagehtml .= "<p>".get_string("nonprocessconfirmationbody", "local_paperattendance") . "</p>";
+			$messagehtml .= "<p>Number of elements in attendanceid". count($attendanceid);
 			foreach ($attendanceid as $pageid){
 				$messagehtml.= "<p>.<a href='" . $CFG->wwwroot . "/local/paperattendance/missingpages.php?action=edit&sesspageid=". $pageid->pageid ."'>" .$pageid->pagenumber. "</a></p>";
 			}
