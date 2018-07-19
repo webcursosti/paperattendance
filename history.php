a0b77ac91bf912b85b7476309433977ec4a550fc
Matias <queiroloco@gmail.com>
2018-07-19 16:34:09 -0400

    fixing checkall postition

--- history.php
+++ history.php
@@ -448,6 +448,7 @@ if( $isteacher || is_siteadmin($USER) || has_capability('local/paperattendance:p
 									WHERE p.sessionid = ? AND p.status = 1";
 				$percentage = $DB->get_record_sql($percentagequery, array($attendance->id, $attendance->id));
 				*/
+				//Query to get the real attendance (without desmatriculated students)
 				$enrolincludes = explode("," ,$CFG->paperattendance_enrolmethod);
 				list ( $sqlin, $param1 ) = $DB->get_in_or_equal ( $enrolincludes );
 				$param2 = array(
@@ -456,27 +457,6 @@ if( $isteacher || is_siteadmin($USER) || has_capability('local/paperattendance:p
 				);
 				$param = array_merge($param2,$param1, $param2, $param1);
 				
-				/*
-				$percentagequery = "SELECT TRUNCATE((COUNT(*)/(SELECT COUNT(*)
-									FROM {paperattendance_presence} AS p
-									INNER JOIN {user} AS u ON (u.id = p.userid)
-									INNER JOIN {user_enrolments} ue ON ue.userid = u.id
-									INNER JOIN {enrol} e ON e.id = ue.enrolid
-									INNER JOIN {role_assignments} ra ON ra.userid = u.id
-									INNER JOIN {context} ct ON ct.id = ra.contextid AND ct.contextlevel = 50
-									INNER JOIN {course} c ON c.id=? AND c.id = ct.instanceid AND e.courseid = c.id
-									INNER JOIN {role} r ON r.id = ra.roleid AND r.shortname = 'student'
-									WHERE p.sessionid = ? AND e.enrol $sqlin AND e.status = 0 AND u.suspended = 0 AND u.deleted = 0)*100),0) AS percentage
-									FROM {paperattendance_presence} AS p
-									INNER JOIN {user} AS u ON (u.id = p.userid)
-									INNER JOIN {user_enrolments} ue ON ue.userid = u.id
-									INNER JOIN {enrol} e ON e.id = ue.enrolid
-									INNER JOIN {role_assignments} ra ON ra.userid = u.id
-									INNER JOIN {context} ct ON ct.id = ra.contextid AND ct.contextlevel = 50
-									INNER JOIN {course} c ON c.id=? AND c.id = ct.instanceid AND e.courseid = c.id
-									INNER JOIN {role} r ON r.id = ra.roleid AND r.shortname = 'student'
-									WHERE p.sessionid = ? AND e.enrol $sqlin AND e.status = 0 AND u.suspended = 0 AND u.deleted = 0 AND p.status = 1";
-				*/
 				$percentagequery = "SELECT TRUNCATE((SUM(p.status)/(COUNT(*))*100),0) AS percentage
 				FROM {paperattendance_presence} AS p
 				INNER JOIN {user} AS u ON (u.id = p.userid)
