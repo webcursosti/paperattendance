c8f442039baa6df1703a79c670827fcdf98a6f3e
Matias <queiroloco@gmail.com>
2019-03-21 03:24:33 -0300

    test date miercoles

--- attendance.php
+++ attendance.php
@@ -92,7 +92,7 @@ if($action == "view"){
 				"token" => $token
 		);
 		// 0 (para domingo) hasta 6 (para sábado)
-		//$fields = array("diaSemana" => 3, "seccionId"=> 59169, "token" => $token);
+		$fields = array("diaSemana" => 3, "seccionId"=> 60801, "token" => $token);
 		
 		curl_setopt($curl, CURLOPT_URL, $url);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
